7a707c76584c48dfb8de2908e206a5f8e88fe19e
Thales Duarte <thales.dfs@gmail.com>
2019-02-24 17:38:44 -0300

    Final

--- App.js
+++ App.js
@@ -5,23 +5,78 @@ import Button from './src/componentes/Button'
 import Display from './src/componentes/Display'
 
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0],
+  current: 0,
+}
+
 export default class App extends Component {
 
-  state = {
-    displayValue: '0'
-  }
+  state = { ...initialState}
+  /*state =  displayValue: '0'
+  }*/
 
   addDigit = n =>{
-    this.setState({displayValue: n})
+    console.debug(typeof this.state.displayValue)//react-native log-android
+   
+   
+   const clearDisplay = this.state.displayValue === '0'
+    || this.state.clearDisplay //se eu colocar varios 0, nao vai acescentando
+
+    if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')){
+      return
+  }
+// this.setState({displayValue: n})
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false})
+
+    if (n != '.'){
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[this.state.current] = newValue
+      this.setState({ values })
+    }
   }
 
   clearMemory = () => {
-    this.setState({displayValue: '0'})
+      this.setState({ ...initialState })
+    //this.setState({displayValue: '0'})
   }
 
   setOperation = operation => {
+      if (this.state.current === 0){//currente é o array 0 e 1, 0 significa que é o primeiro valor
+        this.setState({operation, current: 1, clearDisplay:true })//se clicar em uma operacao, nao apaga numero dod isplay, mas diz que pode apagar quando apertar novo numero
+      }else{
+        const equals = operation === '='
+        const values = [...this.state.values]
 
+        try{
+          values[0] =
+            eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+        } catch (e){
+          values[0] = this.state.values[0]//se o operador é apenas um igual, faz nada tipo "23 = 4" erro
+        }
+
+        values[1] = 0 
+        this.setState({
+          displayValue: `${values[0]}`,//interpolado para sempre ser uma string bug)
+          operation: equals ? null : operation,//se nao apertei o =, muda pro operador novo queeu cliquei
+          current: equals ? 0 : 1,//se apertei =, faz nada
+          clearDisplay: !equals,
+          values,
+        })
+      }
   }
+//const values = [1,20]
+//const operation = '+'
+//eval(`${values[0]} ${operation} ${values[1]}`)
+//21
+//eval('23 + 2')
+//25
 
   render() {
     return (
